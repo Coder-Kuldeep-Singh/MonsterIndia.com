49325dcf3279da20b8d02454795a3fa7b0e1c9d0
mavensingh <kuldeep.avsar@gmail.com>
2020-04-15 00:50:07 +0530

    calling different function for different use

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 
-	"MonsterIndia.com/companies"
+	"MonsterIndia.com/role"
 )
 
 func main() {
-	items, total, err := companies.FindCompaniesByCharacter("z")
-	if err != nil {
-		fmt.Println("failed Companies Function..: ", err)
-	}
-	fmt.Println(total)
-	for _, item := range items {
-		fmt.Println(item)
-	}
+	// items, total, err := companies.FindCompaniesByCharacter("z")
+	// if err != nil {
+	// 	fmt.Println("failed Companies Function..: ", err)
+	// }
+	// fmt.Println(total)
+	// for _, item := range items {
+	// 	fmt.Println(item)
+	// }
+	role.FindJobsByRole()
 	fmt.Println("====================================================================================================")
 
 }
