8912f939218c729117ddca1958a8b2bd05b6c360
mavensingh <kuldeep.avsar@gmail.com>
2020-04-14 15:10:20 +0530

    proxy to handle the client

---
+++ proxy/client.go
@@ -0,0 +1,27 @@
+package proxy
+
+import (
+	"net/http"
+	"time"
+)
+
+func MonsterDomain(URL string) (*http.Response, error) {
+	tr := &http.Transport{
+		MaxIdleConns:        10,
+		MaxIdleConnsPerHost: 20,
+		IdleConnTimeout:     30 * time.Second,
+		DisableCompression:  true,
+	}
+	client := &http.Client{Transport: tr}
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	} else {
+		return resp, nil
+	}
+}
