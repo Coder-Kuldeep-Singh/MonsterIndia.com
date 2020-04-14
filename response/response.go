bf7cea2d97edb8b5a4b9eef964af33ea520b846a
mavensingh <kuldeep.avsar@gmail.com>
2020-04-14 15:10:37 +0530

    response of the page

---
+++ response/response.go
@@ -0,0 +1,31 @@
+package response
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func MonsterResponsePage(selector string, resp *http.Response) (string, error) {
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		fmt.Println("Go query failed!!!", err)
+		return "", err
+	}
+	res, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(res))
+	output := ""
+	doc.Find(selector).Each(func(index int, item *goquery.Selection) {
+		contents, _ := item.Html()
+		output = contents
+	})
+	if output == "" {
+		log.Println("WARNING: Output string is Null!!!")
+	}
+	// fmt.Println(output)
+	return output, nil
+
+}
